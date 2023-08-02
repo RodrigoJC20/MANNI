0f1b207d1ea969421b7c501b83c790a9cd886db4
MisterChief53 <soderol3@hotmail.com>
2023-08-02 11:19:56 -0600

    small progam to test .csv

--- MANNI.cpp
+++ MANNI.cpp
@@ -2,11 +2,19 @@
 //
 
 #include "MANNI.h"
+#include "csv.h"
 
-using namespace std;
 
 int main()
 {
-	cout << "Hello World with CMake" << endl;
+	std::cout << "Hello World with CMake" << std::endl;
+	io::CSVReader<2> in("C:\\Users\\soder\\source\\repos\\RodrigoJC20\\MANNI\\House.csv");
+	in.read_header(io::ignore_extra_column, "sqmt", "price");
+	int sqmt, price;
+	while (in.read_row(sqmt, price)) {
+		std::cout << "sqmt: " << sqmt << " price: " << price << "\n";
+	}
+
+	std::cout << std::endl;
 	return 0;
 }
