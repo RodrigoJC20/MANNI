82daf799c9e34e50d43f4428eee95ca6bccb5de6
Rodrigo Jimenez <rjc.2002vmx@gmail.com>
2023-08-01 22:49:16 -0600

    Initial commit

---
+++ MANNI.cpp
@@ -0,0 +1,12 @@
+﻿// MANNI.cpp : Defines the entry point for the application.
+//
+
+#include "MANNI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World with CMake" << endl;
+	return 0;
+}
